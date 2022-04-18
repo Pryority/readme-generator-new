f02731dfda926124ff3a3f71e2c134264172b11b
Matthew Pryor <matthewapryor@gmail.com>
2022-04-18 09:24:02 -0400

    add validate property to each question object

--- Develop/index.js
+++ Develop/index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -7,56 +9,103 @@ const questions = [
         type: 'input',
         name: 'title',
         message: "Enter the title of the project.",
+        validate: answer => {
+            if (!answer) {
+                return "Enter the title of the project."
+            }
+            return true
+        }
     },
     // DESCRIPTION
     {
         type: 'input',
         name: 'description',
         message: "Enter a description of the project.",
+        validate: answer => {
+            if (!answer) {
+                return "Enter a description of the project."
+            }
+            return true
+        }
     },
     // INSTALLATION
     {
         type: 'input',
         name: 'install',
         message: "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.",
+        validate: answer => {
+            if (!answer) {
+                return "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running."
+            }
+            return true
+        }
     },
     // USAGE
     {
         type: 'input',
         name: 'usage',
         message: "Provide instructions and examples for use. Include screenshots as needed.",
+        validate: answer => {
+            if (!answer) {
+                return "Provide instructions and examples for use. Include screenshots as needed."
+            }
+            return true
+        }
     },
     // LICENSE
     {
         type: 'input',
         name: 'license',
         message: "Enter the license used for this project to let other developers know what they can and cannot do with your project. If you need help choosing a license, refer to https://choosealicense.com.",
+        validate: answer => {
+            if (!answer) {
+                return "Enter the license used for this project to let other developers know what they can and cannot do with your project. If you need help choosing a license, refer to https://choosealicense.com."
+            }
+            return true
+        }
     },
     // CONTRIBUTING
     {
         type: 'input',
         name: 'contribute',
         message: "Include guidelines for how other developers can contribute to this project.",
+        validate: answer => {
+            if (!answer) {
+                return "Include guidelines for how other developers can contribute to this project."
+            }
+            return true
+        }
     },
     // CREDITS
-
     {
         type: 'input',
         name: 'credit',
         message: "List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well.",
+        validate: answer => {
+            if (!answer) {
+                return "List your collaborators, if any, with links to their GitHub profiles. If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section. If you followed tutorials, include links to those here as well."
+            }
+            return true
+        },
     },
     // TESTS
     {
         type: 'input',
         name: 'test',
         message: "Provide examples of how to run tests for the project.",
+        validate: answer => {
+            if (!answer) {
+                return "Provide examples of how to run tests for the project."
+            }
+            return true
+        }
     }
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     const { title, description, install, usage, license, contribute, credit, test } = questions;
-    return `
+    return inquirer.prompt(`
     # <Your-Project-Title>
 
     ## Description
@@ -107,7 +156,7 @@ function writeToFile(fileName, data) {
     ## Tests
 
     ${data.tests}
-    `;
+    `);
 }
 
 // TODO: Create a function to initialize app
